b91fe8a66ddb5bb298395504d3086b80be3d87dc
Serge Boyko <s.boyko@gmail.com>
2026-02-06 12:24:42 +0100

    Initial commit of files (test version)

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# QVCS project
+
+FROM garethflowers/svn-server:latest
+
+# Add only what we need for Git over SSH
+RUN apk add --no-cache git openssh && \
+    adduser -D -h /home/git -s /usr/bin/git-shell git && \
+    mkdir -p /var/opt/git /home/git/.ssh && \
+    chown -R git:git /var/opt/git /home/git && \
+    chmod 700 /home/git/.ssh
+
+# Our entrypoint starts sshd (daemon) + svnserve (foreground)
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+EXPOSE 22 3690
+VOLUME ["/var/opt/svn", "/var/opt/git"]
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+
+# 1) SSH host keys (persist them by mounting /etc/ssh if you want stable fingerprints)
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+  ssh-keygen -A
+fi
+
+# 2) Install git SSH keys (recommended: mount authorized_keys from the host)
+# Expected path: /var/opt/git/keys/authorized_keys
+if [ -f /var/opt/git/keys/authorized_keys ]; then
+  cp /var/opt/git/keys/authorized_keys /home/git/.ssh/authorized_keys
+  chown git:git /home/git/.ssh/authorized_keys
+  chmod 600 /home/git/.ssh/authorized_keys
+fi
+
+# 3) Basic sshd config hardening for a git-only account
+# (keeps image simple: edit inline each start)
+cat > /etc/ssh/sshd_config <<'EOF'
+Port 22
+Protocol 2
+PasswordAuthentication no
+KbdInteractiveAuthentication no
+ChallengeResponseAuthentication no
+LogLevel VERBOSE
+PermitRootLogin no
+PubkeyAuthentication yes
+AuthorizedKeysFile /home/git/.ssh/authorized_keys
+AllowUsers git
+Subsystem sftp internal-sftp
+EOF
+
+# Start sshd (daemonizes by default on Alpine when not using -D)
+# Added -D -e for debug
+/usr/sbin/sshd -D -e
+
+# 4) Start svnserve in foreground (this is the main container process)
+exec /usr/bin/svnserve --daemon --foreground --root /var/opt/svn
